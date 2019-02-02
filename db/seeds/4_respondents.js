379192ecb54e1698b5ac6ba3376e005132e8233c
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-02-02 05:17:03 +0000

    added in respondents seeds and added events ejs + sass

--- db/seeds/4_respondents.js
+++ db/seeds/4_respondents.js
@@ -1,11 +1,11 @@
 exports.seed = function(knex, Promise) {
-  knex('users').del()
+  return knex('respondents').del()
   .then(function () {
     return Promise.all([
-      knex('users').insert({id: 1, name: 'Yuhan Freeman', email: 'yh627220@icloud.com', timeslot_id: 3}),
-      knex('users').insert({id: 2, name: 'Sammyyyy', email: 'sammy@gmail.com', timeslot_id: 2}),
-      knex('users').insert({id: 3, name: 'Nicolas Cage', email: 'nikcage@icloud.com', timeslot_id: 1}),
-      knex('users').insert({id: 4, name: 'Channing Tatum', email: 'ctatum@icloud.com', timeslot_id: 1})
+      knex('respondents').insert({id: 1, name: 'Yuhan Freeman', email: 'yh627220@icloud.com', timeslot_id: 3}),
+      knex('respondents').insert({id: 2, name: 'Sammyyyy', email: 'sammy@gmail.com', timeslot_id: 2}),
+      knex('respondents').insert({id: 3, name: 'Nicolas Cage', email: 'nikcage@icloud.com', timeslot_id: 1}),
+      knex('respondents').insert({id: 4, name: 'Channing Tatum', email: 'ctatum@icloud.com', timeslot_id: 1})
     ]);
   });
 };
