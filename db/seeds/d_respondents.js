6e328ce25bbcd84837a6724e8b7f1fa203cfefed
Maskedspade <nycti_gemimlinz@hotmail.com>
2019-02-04 01:52:02 +0000

    made changes to respondents seeds

--- db/seeds/d_respondents.js
+++ db/seeds/d_respondents.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
   return knex('respondents').del()
   .then(function () {
     return Promise.all([
-      knex('respondents').insert({name: 'Yuhan Freeman', email: 'yh627220@icloud.com', timeslot_identity: 3}),
+      knex('respondents').insert({name: 'Yuhan Freeman', email: 'yh627220@icloud.com', timeslot_identity: 4}),
       knex('respondents').insert({name: 'Sammyyyy', email: 'sammy@gmail.com', timeslot_identity: 2}),
       knex('respondents').insert({name: 'Nicolas Cage', email: 'nikcage@icloud.com', timeslot_identity: 1}),
       knex('respondents').insert({name: 'Channing Tatum', email: 'ctatum@icloud.com', timeslot_identity: 1})
